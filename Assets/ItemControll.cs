92d92021be17fa6b329993c9b12988c15ec4889c
中村　和哉 <kazuya1031@gmail.com>
2017-08-02 01:09:30 +0900

    Lesson６課題
    不要になったアイテムを順次破棄しましょう

---
+++ Assets/ItemControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemControll : MonoBehaviour {
+	protected GameObject unitychan;
+
+
+
+	// Use this for initialization
+	void Start () {
+		this.unitychan = GameObject.Find ("unitychan");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AutoDestroy ();
+
+	}
+
+	public void AutoDestroy(){
+			float difference = unitychan.transform.position.z - this.transform.position.z;
+
+			if (difference > 3) {
+				Destroy (this.gameObject);
+			}  
+
+      }
+
+}
